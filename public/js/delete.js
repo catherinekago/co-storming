b880509022cf20c7de6fac50652ed87fbf08840b
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-11-28 16:25:15 +0100

    Add delete functionality on click (networked)

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,8 +1,31 @@
 AFRAME.registerComponent('delete', {
-    schema: {type: 'string'},
-  
-    init: function () {
-      var stringToLog = this.data;
-      console.log(stringToLog);
-    }
-  });
+
+  init: function () {
+
+    // Touch and Click Input
+    // Set double click detection 
+    this.el.addEventListener("click", function (event) {
+      console.log("click");
+      console.log(event.target)
+      console.log(event.target.parentNode.remove())
+      // delete parent component (post-it)
+    });
+
+    // this.el.addEventListener("touchstart", tapHandler);
+    // // Set double tap detection
+    // // var tapedTwice = false;
+    // function tapHandler(event) {
+    //   // if (!tapedTwice) {
+    //   //   tapedTwice = true;
+    //   //   setTimeout(function () { tapedTwice = false; }, 300);
+    //   //   return false;
+    //   // }
+    //   event.preventDefault();
+    //   console.log('You tapped me!!!');
+    //   // delete parent component (post-it)
+    //   console.log(event.target)
+
+      // Voice Input
+
+  }
+});
