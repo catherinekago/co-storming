646782724762fe8427a42a188b3a1ebd1904f639
mbotsi <77936023+mbotsi@users.noreply.github.com>
2021-11-28 23:13:12 +0100

    Delete public/aframe-keyboard directory

--- public/aframe-keyboard/src/index.js
+++
@@ -1,9 +0,0 @@
-require('./components/aframe-keyboard');
-require('./components/keyboard-button');
-const KeyboardTemplate = require('./keyboardTemplate');
-
-const keyboard = {};
-keyboard.mode = 'normal';
-keyboard.template = new KeyboardTemplate();
-
-module.exports = window.AFK = keyboard;
