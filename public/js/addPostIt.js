1231393f857e6edfe717e47126678bee67ccfb1f
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-11-29 18:41:07 +0100

    Change parent of postits for better synchronization

--- public/js/addPostIt.js
+++ public/js/addPostIt.js
@@ -4,19 +4,14 @@ AFRAME.registerComponent('addpostit', {
        
     // Method to create new entity of post it 
     function createPostIt(x,y,z) {
-        console.log(x +" " +  y + " " + z);
+      console.log(x + " " + y)
         var postIt = document.createElement('a-entity');
         postIt.setAttribute("id", "myPostIt");
         postIt.setAttribute("networked", "template:#post-it-template; attachTemplateToLocal:true");
-        // position of post it x and y divided by x and y positions of wall (y-2.5 because wall is at 2.5 y height)
-        let position = (x / 8) + " " + ((y-2.5)/5)+ " " + "0.51";
+        let position = x + " " + (y)+ " " + "-5.88";
         postIt.setAttribute("position", position);
-        postIt.setAttribute("scale", "0.06 0.1 0.1");
-        document.getElementById("wall").appendChild(postIt);
-
+        document.getElementById("scene").appendChild(postIt);
     }
-
-
 
     // Source Double Click Function: https://stackoverflow.com/questions/5497073/how-to-differentiate-single-click-event-and-double-click-event
       function makeDoubleClick(e) {
