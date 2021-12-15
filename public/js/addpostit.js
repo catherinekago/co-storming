d66bb5fb9c036889e2292bc08d10fa29ca760d94
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-12-15 10:17:29 +0100

    Add Name Tag and text field dummy

--- public/js/addpostit.js
+++ public/js/addpostit.js
@@ -1,15 +1,35 @@
 AFRAME.registerComponent('addpostit', {
+  schema:{
+    userName: {type: "string", default: "Rudolf"},
+    color: {type: "color", default: "red"},
+    isMobile: {type:"string", default: "true"}
+  },
 
   init: function () {
     // Method to create new entity of post it 
-    function createPostIt(x, y) {
+    function addPostIt(x, y) {
       console.log("with click :" + x + " " + y)
       var postIt = document.createElement('a-entity');
       postIt.setAttribute("id", "myPostIt");
       postIt.setAttribute("networked", "template:#post-it-template; attachTemplateToLocal:true");
       let position = x + " " + (y) + " " + "-9.98";
       postIt.setAttribute("position", position);
+
+      // Text Field Component 
+      let textField = document.createElement('a-text');
+      textField.setAttribute("scale", "0.9 0.7 0.7");
+      textField.setAttribute("position", "-0.45 0.1 1000");
+      textField.setAttribute("text", "value", "This is where \n the text will \n go");
+      textField.setAttribute("text", "width", "3");
+      textField.setAttribute("text", "color", "black");
+      postIt.appendChild(textField);
+
       document.getElementById("scene").appendChild(postIt);
+
+      if (!AFRAME.utils.device.isMobile()){
+        // // Marion: this is where you have to make keyboard visible and ready to modify the text field value:
+        // textfield.setAttribute("text", "value", "what the user is typing")
+      }
     }
 
     // Source Double Click Function: https://stackoverflow.com/questions/5497073/how-to-differentiate-single-click-event-and-double-click-event
@@ -29,7 +49,8 @@ AFRAME.registerComponent('addpostit', {
           clearTimeout(timeout);
           let positionX = e.detail.intersection.point.x <= -9.4 ? "-9.4" : (e.detail.intersection.point.x >= 9.4 ? "9.4" : e.detail.intersection.point.x)
           let positionY = e.detail.intersection.point.y >= 11.4 ? "11.4" : (e.detail.intersection.point.y <= 0.56 ? "0.56" : e.detail.intersection.point.y)
-          createPostIt(positionX, positionY)
+          addPostIt(positionX, positionY);
+
           clicks = 0;
         }
       }
