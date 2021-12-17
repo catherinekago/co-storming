061746a55faaddecf3130852216751ad2103c2d6
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-12-17 13:37:40 +0100

    Add basic welcome screen with color selection (no functionality yet)

---
+++ public/js/selectcolor.js
@@ -0,0 +1,53 @@
+AFRAME.registerComponent('selectcolor', {
+    schema: {
+        color: {type: 'color', default: '#000'},
+      },
+
+    init: function () {
+      let color = 
+  
+      this.el.addEventListener("click", function (event) {
+        // delete parent component (post-it)
+        event.stopPropagation();
+        let btns = document.getElementsByClassName("color-btn");
+        for (var i = 0; i < btns.length; i++) {
+            btns.item(i).removeState("selected");
+         }
+        this.addState("selected");
+
+            document.getElementById("welcome-submit").setAttribute("visible", "true");
+            document.getElementById("welcome-submit").classList.add("clickable");
+
+      });
+  
+  
+      // Event listeners to listen for raycaster on delete icon
+      this.el.addEventListener('raycaster-intersected', evt => {
+        this.raycaster = evt.detail.el;
+      });
+      this.el.addEventListener('raycaster-intersected-cleared', evt => {
+        this.raycaster = null;
+      });
+    },
+  
+    tick: function () {
+        if(this.el.is("selected")){
+            this.el.setAttribute("material", "color", this.data.color);
+        } else {
+            this.el.setAttribute("material", "color", "white");
+        }
+      if (!this.raycaster) {
+        return;
+      }  // Not intersecting.
+  
+      let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
+      if (!intersection) {
+        return;
+      }
+      // Create post it if voice command has been provided
+      if(document.getElementById("wall").is("fertig")) {
+        this.el.parentNode.remove();
+        document.getElementById("wall").removeState("fertig");
+      }
+    },
+  });
---
+++ public/js/submitinfo.js
@@ -0,0 +1,38 @@
+AFRAME.registerComponent('submitinfo', {
+
+    init: function () {
+  
+      this.el.addEventListener("click", function (event) {
+        // delete parent component (post-it)
+        event.stopPropagation();
+        event.target.parentNode.remove();
+        document.getElementById("wall").classList.add("clickable");
+      });
+  
+      
+      // Event listeners to listen for raycaster on delete icon
+      this.el.addEventListener('raycaster-intersected', evt => {
+        this.raycaster = evt.detail.el;
+      });
+      this.el.addEventListener('raycaster-intersected-cleared', evt => {
+        this.raycaster = null;
+      });
+  
+    },
+  
+    tick: function () {
+      if (!this.raycaster) {
+        return;
+      }  // Not intersecting.
+  
+      let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
+      if (!intersection) {
+        return;
+      }
+      // Create post it if voice command has been provided
+      if(document.getElementById("wall").is("fertig")) {
+        this.el.parentNode.remove();
+        document.getElementById("wall").removeState("fertig");
+      }
+    },
+  });
